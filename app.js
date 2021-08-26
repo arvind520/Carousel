07516f1eee968a0b4b2a8249fe7e522a81b30bdb
Arvind Chauhan <arvindgta520@gmail.com>
2021-08-26 22:32:53 +0530

    Added some js
    we are starting the image from 2nd position so that on clicking previous btn its move back to img pos 1st

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+var carouselSlide = document.querySelector(".carousel-slide");
+var carouselImages = document.querySelectorAll(".carousel-slide img");
 
+//Buttons
+var prevBtn = document.querySelector("#prevBtn");
+var nextBtn = document.querySelector("#nextBtn");
+
+//Counter
+var counter = 1;
+const size = carouselImages[0].clientWidth;
+// console.log(size);
+carouselSlide.style.transform = "translateX(" + -size + "px)";
